62ffbb7ddf5fedecd443adf6a1377af5f3e2889b
Kade Williams <kwilliams@MTMRec.com>
2026-04-20 10:06:33 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -88,6 +88,7 @@ static bool MatchPattern(string inputLine, string pattern)
         }
         else if (endAnchorPresent)
         {
+            Console.WriteLine("End anchor present");
             if (inputPointer == inputLine.Length - 1)
                 return true;
             return false;
