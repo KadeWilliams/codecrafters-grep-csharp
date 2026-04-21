2c6f1e8f4fe650486332632cae21d94df49e4b4c
Kade Williams <kwilliams@MTMRec.com>
2026-04-21 09:34:05 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -18,7 +18,10 @@ using System.Reflection.Metadata.Ecma335;
 
 static bool MatchHere(string inputLine, int inputPosition, List<IToken> tokens, int tokenPosition, bool endAchorPresent = false)
 {
-    Console.WriteLine(inputLine, inputPosition, tokens, tokenPosition);
+    Console.WriteLine(inputLine);
+    Console.WriteLine(inputPosition);
+    Console.WriteLine(tokens);
+    Console.WriteLine(tokenPosition);
     // we've gotten through all the tokens without failing
     if (tokenPosition == tokens.Count())
     {
