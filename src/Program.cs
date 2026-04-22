0b1dffb04525e70c894472093286484d0069c70e
Kade Williams <kwilliams@MTMRec.com>
2026-04-22 10:01:46 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -106,6 +106,7 @@ static IToken WrapIfQuantifier(string pattern, int index, IToken token, out int 
 static IToken CreateToken(string pattern, int index, out int newIndex)
 {
     newIndex = index;
+    Console.WriteLine($"Checking token: {pattern[newIndex]}");
     if (pattern[newIndex] == '\\')
     {
         switch (pattern[newIndex + 1])
