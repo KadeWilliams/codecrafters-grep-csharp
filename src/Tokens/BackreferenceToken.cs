6e003c5d2e9fab28528b6b46097fa013dfa5f60a
Kade Williams <kwilliams@MTMRec.com>
2026-04-24 11:03:15 -0500

    codecrafters submit [skip ci]

--- src/Tokens/BackreferenceToken.cs
+++ src/Tokens/BackreferenceToken.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace codecrafters_grep.src.Tokens
+namespace codecrafters_grep.src.Tokens;
+
+public class BackreferenceToken : IToken
 {
-    internal class BackreferenceToken
+    private int _position;
+    public BackreferenceToken(int position)
     {
+        _position = position;
     }
+
+    public bool Matches(char c)
+    {
+        return false;
+    }
+    public int Position => _position;
 }
