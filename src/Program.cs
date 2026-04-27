9580d99ed8ce52312cb26e57c4ad95b1fa9dd7ca
Kade Williams <kwilliams@MTMRec.com>
2026-04-27 17:22:10 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -224,6 +224,7 @@ static IToken CreateToken(string pattern, int index, out int newIndex, ref int g
 
 static bool MatchPattern(string inputLine, string pattern)
 {
+    Console.WriteLine(inputLine);
     var startAnchorPresent = false;
     var endAnchorPresent = false;
     var tokens = new List<IToken>();
