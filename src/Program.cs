b9cba46f7360177170a9679ec267ace0d2ea7aaf
Kade Williams <kwilliams@MTMRec.com>
2026-04-28 10:29:51 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -338,6 +338,7 @@ else if (args[0] == "-E")
     {
         var files = args.Skip(2);
         bool includeFileName = false;
+        Console.WriteLine(string.Join(", ", files));
         if (files.Count() < 2)
             includeFileName = true;
         bool lineFound = ProcessFiles(files, pattern, includeFileName);
