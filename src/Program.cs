07f22bc512ef339d4ff52a346fae01cf4f38a4a2
Kade Williams <kwilliams@MTMRec.com>
2026-04-28 11:30:06 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -25,7 +25,7 @@ static bool MatchHere(
     // we've gotten through all of the input characters without passing
     if (inputPosition >= inputLine.Length)
     {
-        if (tokens[tokenPosition] is not ZeroOrOneToken)
+        if (tokens[tokenPosition] is not ZeroOrOneToken || tokens[tokenPosition] is not ZeroOrMoreToken)
         {
             return false;
         }
