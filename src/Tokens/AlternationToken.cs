bef923c4c9fd0b111c731bab702ba95ff5e71ee9
Kade Williams <kwilliams@MTMRec.com>
2026-04-29 11:49:51 -0500

    codecrafters submit [skip ci]

--- src/Tokens/AlternationToken.cs
+++ src/Tokens/AlternationToken.cs
@@ -16,14 +16,6 @@ public class AlternationToken : IToken
 
     public bool Matches(char c)
     {
-        //foreach (var tokenss in _tokens)
-        //{
-        //    foreach (var token in tokenss)
-        //    {
-        //        Console.WriteLine(token.GetType());
-        //    }
-        //}
-        Console.WriteLine($"Class Input: {c}");
         return false;
     }
 
