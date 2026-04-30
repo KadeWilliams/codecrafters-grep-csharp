c19009ee84f8c5a687b0629e4f7ab160f682718d
Kade Williams <kwilliams@MTMRec.com>
2026-04-30 08:15:21 -0500

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -51,6 +51,7 @@ static bool MatchHere(
     if (tokens[tokenPosition] is NQuantifierToken n)
     {
         Console.WriteLine(JsonSerializer.Serialize(n));
+        Console.WriteLine(JsonSerializer.Serialize(n.InnerToken));
         if (n.Number == 0 && n.AtLeastNTimes)
         {
             if (n.MaxNumber is not null) // it has a max value
